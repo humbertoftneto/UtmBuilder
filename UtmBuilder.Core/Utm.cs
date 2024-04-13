0b77d006a11a9dfdd05c6337e360a5f202c81e73
Humberto Ferreira Takemoto Neto <humbertoftneto@gmail.com>
2024-04-13 16:15:16 -0300

    Implementação dos testes unitários.

--- UtmBuilder.Core/Utm.cs
+++ UtmBuilder.Core/Utm.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UtmBuilder.Core.Extensions;
 using UtmBuilder.Core.ValueObjects;
+using UtmBuilder.Core.ValueObjects.Exceptions;
 
 namespace UtmBuilder.Core
 {
@@ -25,6 +26,34 @@ namespace UtmBuilder.Core
         /// Campaign Details
         /// </summary>
         public Campaign Campaign { get; }
+
+        public static implicit operator string(Utm utm)
+            => utm.ToString();
+
+        public static implicit operator Utm(string link)
+        {
+            if (string.IsNullOrEmpty(link))
+                throw new InvalidUrlException();
+
+            var url = new Url(link);
+            var segments = url.Address.Split("?");
+            if (segments.Length == 1)
+                throw new InvalidUrlException("No segments were provided");
+
+            var pars = segments[1].Split("&");
+            var source = pars.Where(x => x.StartsWith("utm_source")).FirstOrDefault("").Split("=")[1];
+            var medium = pars.Where(x => x.StartsWith("utm_medium")).FirstOrDefault("").Split("=")[1];
+            var name = pars.Where(x => x.StartsWith("utm_campaign")).FirstOrDefault("").Split("=")[1];
+            var id = pars.Where(x => x.StartsWith("utm_id")).FirstOrDefault("").Split("=")[1];
+            var term = pars.Where(x => x.StartsWith("utm_term")).FirstOrDefault("").Split("=")[1];
+            var content = pars.Where(x => x.StartsWith("utm_content")).FirstOrDefault("").Split("=")[1];
+
+            var utm = new Utm(
+                new Url(segments[0]), 
+                new Campaign(source, medium, name, id, term, content));
+            return utm;
+        }
+
         public override string ToString()
         {
 
@@ -37,5 +66,17 @@ namespace UtmBuilder.Core
             segments.AddIfNotNull("utm_content", Campaign.Content);
             return $"{Url.Address}?{string.Join("&", segments)}";
         }
+    }
+
+    public class Teste
+    {
+        public void Teste2()
+        {
+            var url =
+                "http://balta.io?utm_source=YouTube&utm_medium=social&utm_campaign=operators";
+
+            string utm = (Utm)url;
+        }
+        
     }
 }
